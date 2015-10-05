3efb17823a39622cd983dfb45f0a700fe6eb5078
Logesh <kumaraguru.logesh@gmail.com>
2015-10-05 06:07:11 +0530

    Initial Commit

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Logesh on 05/10/15.
+//  Copyright (c) 2015 GTALabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Logesh on 05/10/15.
+//  Copyright (c) 2015 GTALabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
