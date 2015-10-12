c07c9951dce8b5d27b2990f7d5db8a002b3676b0
Logesh <kumaraguru.logesh@gmail.com>
2015-10-12 07:03:15 +0530

    Added menu and new layouts

---
+++ AutoLayout/ATHomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATHomeTableViewController.h
+//  AutoLayout
+//
+//  Created by Logesh on 11/10/15.
+//  Copyright (c) 2015 GTALabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATHomeTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ AutoLayout/ATHomeTableViewController.m
@@ -0,0 +1,137 @@
+//
+//  ATHomeTableViewController.m
+//  AutoLayout
+//
+//  Created by Logesh on 11/10/15.
+//  Copyright (c) 2015 GTALabs. All rights reserved.
+//
+
+#import "ATHomeTableViewController.h"
+
+@interface ATHomeTableViewController ()
+
+@end
+
+@implementation ATHomeTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return 4;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    // Configure the cell...
+    
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = @"Equal Height";
+            break;
+        case 1:
+            cell.textLabel.text = @"Equal Width and Height";
+            break;
+        case 2:
+            cell.textLabel.text = @"Diagnol Boxes";
+            break;
+        case 3:
+            cell.textLabel.text = @"Center Alingment";
+            break;
+        default:
+            break;
+    }
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch (indexPath.row) {
+        case 0:
+           [self performSegueWithIdentifier:@"equalHeights" sender:self];
+            break;
+        case 1:
+            [self performSegueWithIdentifier:@"equalWidthAndHeight" sender:self];
+            break;
+        case 2:
+            [self performSegueWithIdentifier:@"diagnolBoxes" sender:self];
+            break;
+        case 3:
+            [self performSegueWithIdentifier:@"centerAlignment" sender:self];
+            break;
+        default:
+            break;
+    }
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
